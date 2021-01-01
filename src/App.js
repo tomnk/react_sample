97a4a85f465586ef69efb19625c2521a13c15fb1
khactam94 <khac.tam.94@gmail.com>
2021-01-01 09:10:41 +0700

    build sample with lodash

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import concat from 'lodash/concat';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          Learn React
+          Learn React {concat([1], 2, [3], [[4]]).join(', ')}
         </a>
       </header>
     </div>
